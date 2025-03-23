90a635565c86300d5bfdb80eb652a6571e8ee128
Morridin <paulza@gmx.de>
2025-03-23 18:35:16 +0100

    Externalise some first parts.

---
+++ lib/__init__.py
@@ -0,0 +1,19 @@
+"""
+This folder shall later contain everything that is not part of the following:
+- User Interface (whichever will be implemented)
+- Main program logic (what you execute when you run Intoxi-Cate).
+- Data
+
+This leaves us with the following:
+- Miscellaneous utils and stuff
+- Algorithms the pipeline needs
+- All the data handling routines
+"""
+
+global snakemake_checkpoints
+
+# Set parts of lib
+__all__ = ["config", "utils"]
+
+# Temporary stuff I later want to get rid of
+__all__ += ["snakemake_checkpoints"]
